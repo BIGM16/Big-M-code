ff4b83d7f09e79d1eaa45a0d292962763cbe4611
Muangala Jonathan <joanthanmuangala@gmail.com>
2025-08-06 02:26:45 +0100

    le formulaire et quelque changement

--- docs/script.js
+++ docs/script.js
@@ -33,11 +33,51 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 // Form submission (prevent default for demo)
-const forms = document.querySelectorAll("form");
-forms.forEach((form) => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Formulaire soumis avec succès ! (Ceci est une démo)");
-    form.reset();
+// const forms = document.querySelectorAll("form");
+// forms.forEach((form) => {
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     alert("Formulaire soumis avec succès ! (Ceci est une démo)");
+//     form.reset();
+//   });
+// });
+
+// Affichage de la div interactif temporaire des ressources
+const resourceDivs = document.querySelectorAll(".ressource");
+const openResourceButtons = document.querySelectorAll(".Ouvre-ressource");
+
+openResourceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    resourceDivs.forEach((div) => {
+      div.classList.add("hidden");
+    });
+    const parentDiv = button.closest(".parent");
+    const resourceDiv = parentDiv.querySelector(".ressource");
+    resourceDiv.classList.toggle("hidden");
   });
+});
+
+// formulaire de soumission candidature
+const contactForm = document.getElementById("contact-form");
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nom = document.getElementById("fullName").value.trim();
+  const numero = document.getElementById("phone").value.trim()
+  const email = document.getElementById("email").value.trim();
+  const cours = document.getElementById("course").value.trim();
+  const formatChoisi = document.querySelector('input[name="format"]:checked').value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  const contactMessage = `Bonjour, je me présente, je suis ${nom}. J'aimerai m'inscrire pour le cours de programmation web en ${cours} sous le format de cours en ${formatChoisi}. J'aimerai soulever un petit quelqur chose à ce propos ${message} et vous prie de me joindre via mon mail ${email} ou directement sur whatsapp à ce numero ${numero}.`;
+
+  const numeroWhatsApp = "243899274502"; // Remplace par ton numéro
+  const lien = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(
+    contactMessage
+  )}`;
+
+  window.open(lien, "_blank"); // Ouvre WhatsApp
+
+  confirmation.style.display = "block"; // Affiche le message
+  form.reset(); // Réinitialise le formulaire
 });
